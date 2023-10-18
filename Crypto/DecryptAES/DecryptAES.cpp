be0ab347399c44ef5d24a38e698deb42faee7143
holdyounger <holdyounger@163.com>
2023-10-18 19:10:08 +0800

    AES解密测试（OpenSSL）

--- Crypto/DecryptAES/DecryptAES.cpp
+++ Crypto/DecryptAES/DecryptAES.cpp
@@ -16,6 +16,8 @@ using namespace std;
 #include <openssl/aes.h>
 #include <openssl/rand.h>
 
+#include "base64.h"
+
 #pragma comment(lib,"libcrypto.lib")
 #pragma comment(lib,"libssl.lib")
 
@@ -208,16 +210,40 @@ unsigned char* str2hex(char* str)
     {
         return 0;
     }
-    ret = (unsigned char*)malloc(str_len / 2);
+    ret = (unsigned char*)malloc((str_len / 2)+1);
     for (i = 0; i < str_len; i = i + 2)
     {
         sscanf(str + i, "%2hhx", &ret[i / 2]);
     }
+    ret[str_len] = '\0';
+    return ret;
+}
+
+//将HEX串的文本进行转换
+char* hex2str(unsigned char* hex)
+{
+    char* ret = NULL;
+    int hex_len = strlen((char*)hex);
+    int i = 0;
+    if ((hex_len % 2) != 0)
+    {
+        return NULL;  // 返回错误值，表示输入的hex字符串长度不正确  
+    }
+    ret = (char*)malloc((hex_len * 2) + 1);  // 分配足够的内存空间以存储原始字符串  
+    memset(ret, 0, (hex_len * 2) + 1);
+    if (ret == NULL) {
+        return NULL;  // 如果内存分配失败，返回错误值  
+    }
+    for (i = 0; i < hex_len * 2; i = i + 2)
+    {
+        sprintf(ret + i, "%02x", hex[i / 2]);
+    }
+    ret[(hex_len * 2)] = '\0';
     return ret;
 }
 
 #define AESKEY "df98b715d5c6ed2b25817b6f255411a1"	//HEX密钥
-#define AESIV "2841ae97419c2973296a0d4bdfe19a4f"	//HEX初始向量
+#define AESIV  "2841ae97419c2973296a0d4bdfe19a4f"	//HEX初始向量
 
 void DecrypyTest(unsigned char* key, std::string strAES)
 {
@@ -252,51 +278,42 @@ int ParseAESTest()
     return 0;
 }
 
-int main() {
-
-    int nVersion = 0;
-    std::vector<unsigned char> salt;
-    std::vector<unsigned char> EncryptionKey;
-    std::vector<unsigned char> pass;
-
-    ParseDatFile("SerializedSystemIni.dat", nVersion, salt, EncryptionKey, pass);
-    std::string strAES = "{AES}tV6KX+O4kjVAtXrnlkR6hcyGA2iAneG2nKG61AVrQBw=";
-
-    unsigned char *buffer;
-
-    buffer = (unsigned char*)malloc(EncryptionKey.size()+1);
-    std::copy(EncryptionKey.begin(), EncryptionKey.end(), buffer);
-
-   // DecrypyTest(buffer, strAES);
-    free(buffer);
-    // {AES}tV6KX+O4kjVAtXrnlkR6hcyGA2iAneG2nKG61AVrQBw=
-    std::string strResult = "";
-    // std::cin >> strAES;
-
-    std::vector<unsigned char> Result(strAES.c_str(), strAES.c_str() + strAES.length() + 1);
-
-    if (nVersion >= 2)
-    {
-        strResult = InvokeDecryptAES(salt, EncryptionKey, pass, Result);
-    }
-
+void EnAndDecrpytDemo()
+{
     AES_KEY encryptkey;
     AES_KEY decryptkey;
 
     unsigned char* key;
     unsigned char* stdiv;
+    char* keyDe;
+    char* stdivDe;
 
-    // key = str2hex((char*)EncryptionKey.data());
     key = str2hex((char*)AESKEY);
     stdiv = str2hex((char*)AESIV);
+
+    {
+        printf("hex1.%s\n", key);
+        printf("hex2.%s\n", stdiv);
+
+        keyDe = hex2str(key);
+        stdivDe = hex2str(stdiv);
+
+        printf("1.%s\n", keyDe);
+        printf("2.%s\n", stdivDe);
+
+        free(keyDe);
+        free(stdivDe);
+    }
+
     AES_set_encrypt_key(key, 128, &encryptkey);
     AES_set_decrypt_key(key, 128, &decryptkey);
 
     unsigned char plain_text[32];
 
+    //需要将加密区块长度填充为16字节整数倍，此处使用zero-padding，即末尾全用0填充
     memcpy(plain_text, "AES encrypt in openssl demo", 27);
     memset(plain_text + 27, 0, 5);
-    //需要将加密区块长度填充为16字节整数倍，此处使用zero-padding，即末尾全用0填充
+
     printf("plain_text: ");
     for (int i = 0; i < 32; i++)
     {
@@ -316,14 +333,12 @@ int main() {
     for (int i = 0; i < 32; i++)
     {
         printf(" % 02X ", encrypted_text[i]);
-    }   
-    
+    }
     printf("\n");
 
-    for (int i = 0; i < 32; i++)
-    {
-        printf(" %c", encrypted_text[i]);
-    }
+    char* plainEncrypText;
+    plainEncrypText = hex2str(encrypted_text);
+    printf("\nplainEncrypText: %s", plainEncrypText);
     printf("\n");
 
     unsigned char decrypted_text[32];
@@ -339,6 +354,164 @@ int main() {
     }
 
     printf("\n %s", decrypted_text);
+}
+
+/*
+* @fn			AES_128_Decode
+* @brief		AES解密封装接口，仅支持AES 128
+* @param[in]
+*   key: 密钥
+*   tmpiv: 偏移量
+*   origin: 待解密值
+* @param[out]   
+* @return		返回解密后的内容
+*
+* @detail      	
+* @author		mingming.shi
+* @date			
+*/
+unsigned char* AES_128_Encode(unsigned char* key, unsigned char* tmpiv, unsigned char* origin)
+{
+    AES_KEY decryptkey;
+
+    // unsigned char* HexKey = str2hex(key);
+
+    AES_set_encrypt_key(key, 128, &decryptkey);
+
+    unsigned char* output_text;
+    output_text = (unsigned char*)malloc(sizeof(128));
+    memset(output_text, '\0', 128);
+    AES_cbc_encrypt(origin, output_text, 32, &decryptkey, tmpiv, AES_ENCRYPT);
+
+    return output_text;
+}
+
+/*
+* @fn			AES_128_Decode
+* @brief		AES解密封装接口，仅支持AES 128
+* @param[in]
+*   key: 密钥
+*   tmpiv: 偏移量
+*   origin: 待解密值
+* @param[out]   
+* @return		返回解密后的内容
+*
+* @detail      	
+* @author		mingming.shi
+* @date			
+*/
+unsigned char* AES_128_Decode(unsigned char* key, unsigned char *tmpiv, unsigned char* origin)
+{
+    AES_KEY aesKey;
+
+    // unsigned char* HexKey = str2hex(key);
+
+    AES_set_decrypt_key(key, 128, &aesKey);
+
+    unsigned char *output_text;
+    output_text = (unsigned char*)malloc(sizeof(128));
+    memset(output_text, '\0', 128);
+    AES_cbc_encrypt(origin, output_text, 32, &aesKey, tmpiv, AES_DECRYPT);
+
+    return output_text;
+}
+
+void DeCodeWebLogic()
+{
+    // 获取key
+    int nVersion = 0;
+    std::vector<unsigned char> salt;
+    std::vector<unsigned char> EncryptionKey;
+    std::vector<unsigned char> pass;
+
+    ParseDatFile("SerializedSystemIni.dat", nVersion, salt, EncryptionKey, pass);
+
+    if (nVersion >= 2)
+    {
+        unsigned char* strAESKey;
+
+        strAESKey = (unsigned char*)malloc(EncryptionKey.size() + 1);
+        memset(strAESKey, '\0', EncryptionKey.size() + 1);
+        std::copy(EncryptionKey.begin(), EncryptionKey.end(), strAESKey);
+
+        printf("AESKey Value:%s\n", strAESKey);
+
+
+        // 获取base64解码后的文本
+        char* EncryptText = TestBase64();
+        {
+            printf("AES Origin Value:%s\n", EncryptText);
+            for (int i = 0; i < strlen(EncryptText); i++)
+            {
+                // printf(" % 02X ", EncryptText[i]);
+            }
+            std::cout << "\n-------------------------\n";
+        }
+
+        // 获取文本的hex形式
+        char* HexAESOrigin = hex2str((unsigned char *)EncryptText);
+
+        // 获取偏移量
+        unsigned char ivec[16];
+        unsigned char* iv;
+        iv = (unsigned char*)malloc(16);
+        memset(iv, 0, 16);
+        memcpy(iv, HexAESOrigin, 16);
+        unsigned char* TempIv = str2hex((char*)iv);
+        memcpy(ivec, TempIv, 16);
+
+        free(TempIv);
+        free(HexAESOrigin);
+
+        unsigned char plain_text[32];
+
+        //需要将加密区块长度填充为16字节整数倍，此处使用zero-padding，即末尾全用0填充
+        memcpy(plain_text, "AES encrypt in openssl demo", 27);
+        memset(plain_text + 27, 0, 5);
+
+        unsigned char* enpwd = AES_128_Encode(strAESKey, ivec, (unsigned char*)plain_text);
+        unsigned char* depwd = AES_128_Decode(strAESKey, ivec, (unsigned char*)enpwd);
+        printf("pwd:%s\n", depwd);
+    }
+    
+}
+
+int main() {
+
+    EnAndDecrpytDemo();
+
+    DeCodeWebLogic();
+
+
+    int nVersion = 0;
+    std::vector<unsigned char> salt;
+    std::vector<unsigned char> EncryptionKey;
+    std::vector<unsigned char> pass;
+
+    ParseDatFile("SerializedSystemIni.dat", nVersion, salt, EncryptionKey, pass);
+    std::string strAES = "{AES}tV6KX+O4kjVAtXrnlkR6hcyGA2iAneG2nKG61AVrQBw=";
+
+    unsigned char *AESKey;
+
+    AESKey = (unsigned char*)malloc(EncryptionKey.size()+1);
+    std::copy(EncryptionKey.begin(), EncryptionKey.end(), AESKey);
+
+    char* plainEncry = hex2str(AESKey);
+
+    printf("plainEncry:%s", plainEncry);
+
+   // DecrypyTest(buffer, strAES);
+    free(AESKey);
+    // {AES}tV6KX+O4kjVAtXrnlkR6hcyGA2iAneG2nKG61AVrQBw=
+    std::string strResult = "";
+    // std::cin >> strAES;
+
+    std::vector<unsigned char> Result(strAES.c_str(), strAES.c_str() + strAES.length() + 1);
+
+    if (nVersion >= 2)
+    {
+        strResult = InvokeDecryptAES(salt, EncryptionKey, pass, Result);
+    }
 
     return 0;
 }
