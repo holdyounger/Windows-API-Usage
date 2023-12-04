1843b5c457814fae0c5be7d584d9371f9180ffc1
holdyounger <88082435+holdyounger@users.noreply.github.com>
2023-12-04 17:53:22 +0800

    rpc相关函数调用示例

--- RPC/RPCDemo/Client/rpcclient.cpp
+++ RPC/RPCDemo/Client/rpcclient.cpp
@@ -11,9 +11,44 @@ using namespace std;
 
 void doRpcCall();
 
+int Start_Ncacn_np(unsigned char* pszNetworkAddr, unsigned char** pszStringBinding)
+{
+    RPC_STATUS status = 0;
+
+    status = RpcStringBindingCompose(NULL,
+        (unsigned char*)"ncacn_np",
+        pszNetworkAddr,
+        (unsigned char*)"\\pipe\\hello",
+        NULL,
+        pszStringBinding);
+    if (status != 0) {
+        cout << "RpcStringBindingCompose returns: " << status << "!" << endl;
+        return -1;
+    }
+
+}
+
+int Start_NcalRpc(unsigned char* pszNetworkAddr, unsigned char** pszStringBinding)
+{
+    RPC_STATUS status = 0;
+
+    status = RpcStringBindingCompose(NULL,
+        (unsigned char*)"ncalrpc",
+        pszNetworkAddr,
+        (unsigned char*)"AppName",
+        NULL,
+        pszStringBinding);
+    if (status != 0) {
+        cout << "RpcStringBindingCompose returns: " << status << "!" << endl;
+        return -1;
+    }
+
+}
+
 int main(int argc, char** argv)
 {
     int i = 0;
+    int nType = 0;
     RPC_STATUS status = 0;
 
     unsigned char* pszNetworkAddr = NULL;
@@ -26,15 +61,25 @@ int main(int argc, char** argv)
         }
     }
 
-    status = RpcStringBindingCompose(NULL,
-        (unsigned char*)"ncacn_np",
-        pszNetworkAddr,
-        (unsigned char*)"\\pipe\\hello",
-        NULL,
-        &pszStringBinding);
-    if (status != 0) {
-        cout << "RpcStringBindingCompose returns: " << status << "!" << endl;
-        return -1;
+
+    cout << "1: ncacn_np; 2. ncalrpc\nselect:";
+
+    cin >> nType;
+
+    switch (nType)
+    {
+    case 1:
+    {
+        Start_Ncacn_np(pszNetworkAddr, &pszStringBinding);
+        break;
+    }
+    case 2:
+    {
+        Start_NcalRpc(pszNetworkAddr, &pszStringBinding);
+        break;
+    }
+    default:
+        break;
     }
 
     cout << "pszStringBinding = " << pszStringBinding << endl;
--- RPC/RPCDemo/Server/rpcserver.cpp
+++ RPC/RPCDemo/Server/rpcserver.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 #include "../RpcComplier/hello_h.h"
-
+#include "DebugTrace.h"
+#include <sddl.h>
 #pragma comment(lib,"Rpcrt4.lib")
 
 
-int main(void)
+int g_nType = 0;
+RPC_BINDING_HANDLE g_RpcHandle = NULL;
+
+
+int Start_Ncacn_np()
 {
 	RPC_STATUS status = 0;
 
@@ -40,7 +45,124 @@ int main(void)
 	}
 
 	cin.get();
+}
+
+int Start_NcalRpc()
+{
+	RPC_STATUS status = 0;
+	unsigned int mincall = 1;
+	unsigned int maxcall = 20;
+
+	RpcServerUseProtseqEp(
+		(unsigned char*)"ncalrpc",
+		RPC_C_PROTSEQ_MAX_REQS_DEFAULT,
+		(unsigned char*)"AppName",
+		NULL);
+
+	if (status != 0) {
+		cout << "RpcServerUseProtseqEp returns: " << status << endl;
+		return -1;
+	}
+
+	status = RpcServerRegisterIf(
+		hello_v1_0_s_ifspec,
+		NULL,
+		NULL);
+	if (status != 0) {
+		cout << "RpcServerRegisterIf returns: " << status << endl;
+		return -1;
+	}
+
+	cout << "Rpc Server Begin Listening..." << endl;
+	status = RpcServerListen(mincall, maxcall, FALSE);
+	if (status != 0) {
+		cout << "RpcServerListen returns: " << status << endl;
+		return -1;
+	}
+
+	cin.get();
+}
+
+int main(void)
+{
+	cout << "1: ncacn_np; 2. ncalrpc\nSelect:";
+	cin >> g_nType;
+
+	switch (g_nType)
+	{
+	case 1:
+	{
+		Start_Ncacn_np();
+		break;
+	}
+	case 2:
+	{
+		Start_NcalRpc();
+		break;
+	}
+	default:
+		break;
+	}
+	
 	return 0;
+}
+
+std::wstring GetClientSIDString()
+{
+	std::wstring clientSID = L"S-1-0-0";
+
+	RPC_STATUS status = RpcImpersonateClient(nullptr);
+	if (status != RPC_S_OK)
+	{
+		Debug_Error(_T("RpcImpersonateClient failed during GetClientSIDString"), status);
+		return clientSID;
+	}
+
+	HANDLE hToken = nullptr;
+	if (!OpenThreadToken(GetCurrentThread(), TOKEN_ALL_ACCESS, true, &hToken))
+	{
+		Debug_Error(_T("OpenThreadToken failed during GetClientSIDString"));
+	}
+	else
+	{
+		DWORD dwSize = 0;
+		if (!GetTokenInformation(hToken, TokenUser, nullptr, 0, &dwSize) && GetLastError() != ERROR_INSUFFICIENT_BUFFER)
+		{
+			Debug_Error(_T("failed to get token information size"));
+		}
+		else
+		{
+			PTOKEN_USER tokenUser = (PTOKEN_USER)LocalAlloc(LPTR, dwSize);
+			if (tokenUser == nullptr)
+			{
+				Debug_Error(_T("failed to allocate token information buffer"));
+			}
+			else
+			{
+				if (!GetTokenInformation(hToken, TokenUser, tokenUser, dwSize, &dwSize))
+				{
+					Debug_Error(_T("failed to get token information"));
+				}
+				else
+				{
+					LPWSTR sidString = nullptr;
+					if (!ConvertSidToStringSidW(tokenUser->User.Sid, &sidString))
+					{
+						Debug_Error(_T("failed to convert sid to string"));
+					}
+					clientSID.assign(sidString);
+				}
+
+				LocalFree(tokenUser);
+			}
+		}
+
+		CloseHandle(hToken);
+	}
+
+	RevertToSelf();
+
+	return clientSID;
 }
 
 /************************************************************************/
@@ -63,7 +185,69 @@ void __RPC_USER MIDL_user_free(void* ptr)
 
 void HelloProc(unsigned char* szhello)
 {
-	cout << szhello << endl;
+
+	unsigned int Pid;
+	if (!g_RpcHandle)
+	{
+		RPC_STATUS rpcs = RpcServerInqBindingHandle(&g_RpcHandle);
+
+		if (rpcs != RPC_S_OK)
+		{
+			cout << "Get Handle Failed" << endl;
+			exit(-1);
+		}
+		
+		cout << "Get handle:" << hex << &g_RpcHandle << endl;
+	}
+
+	if (1)
+	{
+		if (g_nType == 2)
+		{
+			RPC_STATUS rpcs = RpcServerInqBindingHandle(&g_RpcHandle);
+
+			if (rpcs != RPC_S_OK)
+			{
+				cout << "Get Handle Failed" << endl;
+				exit(-1);
+			}
+
+			cout << "Get handle:" << hex << g_RpcHandle << endl;
+			unsigned int Pid;
+			RPC_STATUS rpcStas = I_RpcBindingInqLocalClientPID(g_RpcHandle, (unsigned long*)&Pid);
+			if (rpcs != RPC_S_OK)
+			{
+				cout << "Get Handle Failed" << endl;
+				exit(-1);
+			}
+
+			cout << "Get Pid:" << dec << Pid << endl;
+		}
+
+		if (0 == strcmp((const char*)szhello,"sid"))
+		{
+			std::wstring sid = GetClientSIDString();
+			wcout << L"sid:" << sid << endl;
+		}
+	}
+
+	if(0)
+	{
+		RPC_CALL_ATTRIBUTES CallAttributes;  // this maps to RPC_CALL_ATTRIBUTES_V1
+
+		memset(&CallAttributes, 0, sizeof(CallAttributes));
+		CallAttributes.Version = RPC_CALL_ATTRIBUTES_VERSION;    // maps to 1
+		CallAttributes.Flags = RPC_QUERY_CLIENT_PRINCIPAL_NAME | RPC_QUERY_CALL_LOCAL_ADDRESS;//....
+		RPC_STATUS Status = RpcServerInqCallAttributes(0, &CallAttributes);
+
+		if (Status != RPC_S_OK)
+		{
+			cout << "Get Pid Failed" << endl;
+			exit(-1);
+		}
+
+		cout << "Pid:" << 1 << "\tMsg:" << szhello << endl;
+	}
 }
 
 void Shutdown(void)
